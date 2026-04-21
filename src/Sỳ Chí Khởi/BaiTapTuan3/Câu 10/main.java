f3cc4f9c4eb437a1a523e78974a6aadb91748b13
Sỳ <148116115+Molten-2168@users.noreply.github.com>
2026-04-21 17:58:25 +0700

    labubu

--- src/Sỳ Chí Khởi/BaiTapTuan3/Câu 10/main.java
+++ src/Sỳ Chí Khởi/BaiTapTuan3/Câu 10/main.java
@@ -6,24 +6,12 @@ import truongdaihoc.TruongDaiHoc;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc   new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        DanhSachTS quanLy = new DanhSachTS();
         
-        // Khoi tao quan ly danh sach (da co san 2 thi sinh co dinh ben trong)
-        DanhSachTS quanLy   new DanhSachTS();
-        
-        // Nhap danh sach thi sinh moi tu ban phim
-        System.out.println("   NHAP DANH SACH THI SINH TUY CHINH   ");
         quanLy.nhapDanhSach(sc);
         
-        // Khoi tao thong tin truong dai hoc
-        TruongDaiHoc hsu   new TruongDaiHoc("HSU", 15.0);
-        
-        // 1. Xuat toan bo ket qua xet tuyen (ca trung va truot)
-        System.out.println("\n--- TIEN HANH XET TUYEN ---");
-        quanLy.xetTuyenTongHop(hsu);
-        
-        // 2. Chi xuat nhung thi sinh thuc su trung tuyen
-        System.out.println("\n--- LOC DANH SACH TRUNG TUYEN CUOI CUNG ---");
+        TruongDaiHoc hsu = new TruongDaiHoc("HSU", 15.0);
         quanLy.xuatDanhSachTrungTuyen(hsu);
         
         sc.close();
