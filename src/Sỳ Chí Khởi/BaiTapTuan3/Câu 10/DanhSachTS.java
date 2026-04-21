e75db12d6eee9da8c730731e5e26481e1f620dc2
Sỳ <148116115+Molten-2168@users.noreply.github.com>
2026-04-21 18:02:52 +0700

    labubu

--- src/Sỳ Chí Khởi/BaiTapTuan3/Câu 10/DanhSachTS.java
+++ src/Sỳ Chí Khởi/BaiTapTuan3/Câu 10/DanhSachTS.java
@@ -9,39 +9,33 @@ public class DanhSachTS {
 
     public DanhSachTS() {
         dsCoDinh = new ThiSinh[2];
-        dsCoDinh[0] = new ThiSinh("Nguyen Van Co Dinh", "CD01", 7.0, 8.0, 9.0, true, false);
-        dsCoDinh[1] = new ThiSinh("Le Thi Co Dinh", "CD02", 5.0, 5.0, 5.0, false, true);
+        dsCoDinh[0] = new ThiSinh("Nguyen Van A", "CD01", 7.0, 8.0, 9.0, true, false);
+        dsCoDinh[1] = new ThiSinh("Le Thi B", "CD02", 4.0, 5.0, 4.0, false, true);
     }
 
     public void nhapDanhSach(Scanner sc) {
         System.out.print("Nhap so luong thi sinh can them: ");
         int n = sc.nextInt();
         sc.nextLine(); 
-        
         dsNhapVao = new ThiSinh[n]; 
-        
         for (int i = 0; i < n; i++) {
-            System.out.println("NHAP THONG TIN THI SINH ");
+            System.out.println("--- Nhap thi sinh moi " + (i + 1) + " ---");
             dsNhapVao[i] = new ThiSinh();
             dsNhapVao[i].nhap(sc);
         }
     }
 
     public void xuatDanhSachTrungTuyen(TruongDaiHoc truong) {
-        System.out.println("\n   DANH SACH THI SINH TRUNG TUYEN VAO " + truong.getTenTruong().toUpperCase() + "   ");
-
-        // Kiem tra danh sach co dinh
-        for (int i = 0; i < dsCoDinh.length; i++) {
-            if (dsCoDinh[i].tinhDiemXetTuyen() >= truong.getDiemChuan() && !dsCoDinh[i].kiemTraDiemLiet()) {
-                System.out.println("+ " + dsCoDinh[i].getHoTen() + " (SBD: " + dsCoDinh[i].getSbd() + ") - Diem: " + dsCoDinh[i].tinhDiemXetTuyen());
+        System.out.println("\n--- DANH SACH TRUNG TUYEN VAO " + truong.getTenTruong() + " ---");
+        for (ThiSinh ts : dsCoDinh) {
+            if (ts.tinhDiemXetTuyen() >= truong.getDiemChuan() && !ts.kiemTraDiemLiet()) {
+                System.out.println("+ " + ts.getHoTen() + " (SBD: " + ts.getSbd() + ")");
             }
         }
-
-        // Kiem tra danh sach nhap vao
         if (dsNhapVao != null) {
-            for (int i = 0; i < dsNhapVao.length; i++) {
-                if (dsNhapVao[i].tinhDiemXetTuyen() >= truong.getDiemChuan() && !dsNhapVao[i].kiemTraDiemLiet()) {
-                    System.out.println("+ " + dsNhapVao[i].getHoTen() + " (SBD: " + dsNhapVao[i].getSbd() + ") - Diem: " + dsNhapVao[i].tinhDiemXetTuyen());
+            for (ThiSinh ts : dsNhapVao) {
+                if (ts.tinhDiemXetTuyen() >= truong.getDiemChuan() && !ts.kiemTraDiemLiet()) {
+                    System.out.println("+ " + ts.getHoTen() + " (SBD: " + ts.getSbd() + ")");
                 }
             }
         }
