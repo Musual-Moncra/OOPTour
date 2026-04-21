425965bd6d50ac6c0738930276b4bfc25792831e
Sỳ <148116115+Molten-2168@users.noreply.github.com>
2026-04-21 17:38:41 +0700

    labubu

--- src/Sỳ Chí Khởi/BaiTapTuan3/Câu 10/ThiSinh.java
+++ src/Sỳ Chí Khởi/BaiTapTuan3/Câu 10/ThiSinh.java
@@ -1,7 +1,6 @@
 package thisinh;
 
 import java.util.Scanner;
-import truongdaihoc.TruongDaiHoc;
 
 public class ThiSinh {
     private String hoTen;
@@ -12,73 +11,55 @@ public class ThiSinh {
     private boolean hsGioi;
     private boolean uuTien;
 
+    // Constructor rong
     public ThiSinh() {
+    }
+
+    // Constructor day du tham so de tao doi tuong nhanh
+    public ThiSinh(String hoTen, String sbd, double diem1, double diem2, double diem3, boolean hsGioi, boolean uuTien) {
+        this.hoTen   hoTen;
+        this.sbd   sbd;
+        this.diem1   diem1;
+        this.diem2   diem2;
+        this.diem3   diem3;
+        this.hsGioi   hsGioi;
+        this.uuTien   uuTien;
     }
 
     public String getHoTen() {
         return hoTen;
     }
 
+    public String getSbd() {
+        return sbd;
+    }
+
     public double tinhDiemXetTuyen() {
-        double tong = diem1 + diem2 + diem3;
+        double tong   diem1 + diem2 + diem3;
         if (hsGioi) tong += 1.0;
         if (uuTien) tong += 0.5;
         return tong;
     }
 
     public boolean kiemTraDiemLiet() {
-        if (diem1 == 0 || diem2 == 0 || diem3 == 0) {
-            return true;
-        }
-        return false;
+        return (diem1   0 || diem2   0 || diem3   0);
     }
 
     public void nhap(Scanner sc) {
         System.out.print("Nhap ho ten: ");
-        this.hoTen = sc.nextLine();
+        this.hoTen   sc.nextLine();
         System.out.print("Nhap SBD: ");
-        this.sbd = sc.nextLine();
+        this.sbd   sc.nextLine();
         System.out.print("Nhap diem mon 1: ");
-        this.diem1 = sc.nextDouble();
+        this.diem1   sc.nextDouble();
         System.out.print("Nhap diem mon 2: ");
-        this.diem2 = sc.nextDouble();
+        this.diem2   sc.nextDouble();
         System.out.print("Nhap diem mon 3: ");
-        this.diem3 = sc.nextDouble();
-        System.out.print("HS Gioi (1 - Co, 0 - Khong): ");
-        this.hsGioi = (sc.nextInt() == 1);
-        System.out.print("Uu tien (1 - Co, 0 - Khong): ");
-        this.uuTien = (sc.nextInt() == 1);
-        sc.nextLine();
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        ThiSinh ts1 = new ThiSinh();
-        ThiSinh ts2 = new ThiSinh();
-        ThiSinh ts3 = new ThiSinh();
-
-        System.out.println("   NHAP THONG TIN THI SINH 1   ");
-        ts1.nhap(sc);
-        System.out.println("   NHAP THONG TIN THI SINH 2   ");
-        ts2.nhap(sc);
-        System.out.println("   NHAP THONG TIN THI SINH 3   ");
-        ts3.nhap(sc);
-
-        TruongDaiHoc truongHSU = new TruongDaiHoc("HSU", 15.0);
-        TruongDaiHoc truongHUTECH = new TruongDaiHoc("HUTECH", 16.0);
-        TruongDaiHoc truongSGU = new TruongDaiHoc("SGU", 15.5);
-
-        System.out.println("\n   KET QUA XET TUYEN   ");
-        System.out.println("Xet tuyen Thi sinh 1 vao HSU:");
-        truongHSU.xetTuyen(ts1);
-        
-        System.out.println("\nXet tuyen Thi sinh 2 vao HUTECH:");
-        truongHUTECH.xetTuyen(ts2);
-        
-        System.out.println("\nXet tuyen Thi sinh 3 vao SGU:");
-        truongSGU.xetTuyen(ts3);
-
-        sc.close();
+        this.diem3   sc.nextDouble();
+        System.out.print("Co phai HS Gioi khong (1-Co, 0-Khong): ");
+        this.hsGioi   (sc.nextInt()   1);
+        System.out.print("Co thuoc dien uu tien khong (1-Co, 0-Khong): ");
+        this.uuTien   (sc.nextInt()   1);
+        sc.nextLine(); // Clear buffer
     }
 }
